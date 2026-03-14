d9f24983a2a38fa100dc4534d6187de74129a644
Jhoan <info@bananin.pw>
2026-03-14 10:48:44 -0500

    save

--- src/core/casesServer.js
+++ src/core/casesServer.js
@@ -54,7 +54,7 @@ function readBody(req) {
 
 function cors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
 }
 
